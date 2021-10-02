6f47b790fc3d7e467dc601c9f470417094857a1d
Alexander Niebuhr <tech@nbhr.io>
2021-10-02 13:59:19 +0200

    chore: :arrow_up: bump deps

--- src/debugger/debugger.ts
+++ src/debugger/debugger.ts
@@ -1,6 +1,6 @@
 interface LogOptions {
   type: string
 }
-export function createLog (text: string, options?: LogOptions): void {
+export function createLog(text: string, options?: LogOptions): void {
   console.log(text)
 }
